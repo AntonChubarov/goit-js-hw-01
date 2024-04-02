7eeed3fe518453992975c1b058bec3b1b9170492
AntonChubarov <achlp600@gmail.com>
2024-04-02 09:09:57 -0400

    feat: task 1

---
+++ log/log.js
@@ -0,0 +1,12 @@
+(function() {
+  var oldLog = console.log;
+  var outputDiv = document.getElementById('console-output');
+
+  console.log = function(message) {
+    if (typeof message == 'object') {
+      message = JSON.stringify(message);
+    }
+    outputDiv.innerHTML += '<p>' + message + '</p>';
+    oldLog.apply(console, arguments);
+  }
+})();
